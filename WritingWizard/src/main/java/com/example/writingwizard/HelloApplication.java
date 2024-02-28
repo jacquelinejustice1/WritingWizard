e9ade85f3ad4a099b684f3b5bc5eb894e61b2fee
Jacqueline Justice <kobrakid3716@gmail.com>
2024-02-28 12:25:15 -0600

    Changed the size of the application Added a text box for the user Added a border for the format options

--- WritingWizard/src/main/java/com/example/writingwizard/HelloApplication.java
+++ WritingWizard/src/main/java/com/example/writingwizard/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -11,8 +12,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.getIcons().add(new Image("C:\\Users\\jjust\\Documents\\CSCI 4600\\WritingWizard\\Wizard2.png"));
+        //"C:\Users\jjust\Documents\CSCI 4600\WritingWizard\Wizard2.png"
+        Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
+        //scene.getStylesheets().add("C:\\Users\\jjust\\Documents\\CSCI 4600\\WritingWizard\\WritingWizard\\src\\main\\java\\com\\example\\writingwizard\\stylesheet.css");
+        stage.setTitle("Writing Wizard");
         stage.setScene(scene);
         stage.show();
     }
