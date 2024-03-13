baae4d845f96a359154c90a8dcff53fac198ca5d
Jacqueline Justice <kobrakid3716@gmail.com>
2024-03-13 12:11:57 -0500

    Changed the size of the application Added a text box for the user Added a border for the format options

--- WritingWizard/src/main/java/com/example/writingwizard/HelloApplication.java
+++ WritingWizard/src/main/java/com/example/writingwizard/HelloApplication.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 import java.io.IOException;
 
@@ -12,13 +15,32 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        stage.getIcons().add(new Image("C:\\Users\\jjust\\Documents\\CSCI 4600\\WritingWizard\\Wizard2.png"));
+        //stage.getIcons().add(new Image("WritingWizard\\Images\\Wizard2.png"));
+
         //"C:\Users\jjust\Documents\CSCI 4600\WritingWizard\Wizard2.png"
         Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
         //scene.getStylesheets().add("C:\\Users\\jjust\\Documents\\CSCI 4600\\WritingWizard\\WritingWizard\\src\\main\\java\\com\\example\\writingwizard\\stylesheet.css");
         stage.setTitle("Writing Wizard");
         stage.setScene(scene);
         stage.show();
+
+
+        //login in
+        FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+
+        Scene scene2 = new Scene(fxmlLoader2.load(),1366, 768);
+        stage.setTitle("Writing Wizard");
+        stage.setScene(scene2);
+        stage.show();
+
+        //create account
+        FXMLLoader fxmlLoader3 = new FXMLLoader(HelloApplication.class.getResource("create-account1.fxml"));
+
+        Scene scene3 = new Scene(fxmlLoader3.load(),1366, 768);
+        stage.setTitle("Writing Wizard");
+        stage.setScene(scene3);
+        stage.show();
+
     }
 
     public static void main(String[] args) {
