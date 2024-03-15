e2c5981325b3f7d586ce1da6b52f0836bdd458a9
Jacqueline Justice <kobrakid3716@gmail.com>
2024-03-15 12:44:58 -0500

    got rid of the errors i pushed

--- WritingWizard/src/main/java/com/example/writingwizard/HelloController.java
+++ WritingWizard/src/main/java/com/example/writingwizard/HelloController.java
@@ -1,8 +1,23 @@
 package com.example.writingwizard;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
 
+    public void documentName(ActionEvent actionEvent) {
+    }
+
+    public void boldText(ActionEvent actionEvent) {
+    }
+
+    public void italicText(ActionEvent actionEvent) {
+    }
+
+    public void underlineText(ActionEvent actionEvent) {
+    }
+
+    public void strikeText(ActionEvent actionEvent) {
+    }
 }
