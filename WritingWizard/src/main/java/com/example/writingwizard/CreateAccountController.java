ea815f9269a3d63f0f4b002b14f6fa1c5f1cdbbb
Jacqueline Justice <kobrakid3716@gmail.com>
2024-03-15 12:43:20 -0500

    Added OnAction events for the login and createaccount fxml

--- WritingWizard/src/main/java/com/example/writingwizard/CreateAccountController.java
+++ WritingWizard/src/main/java/com/example/writingwizard/CreateAccountController.java
@@ -1,7 +1,26 @@
 package com.example.writingwizard;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class CreateAccountController {
+    public String createNewUsername(ActionEvent actionEvent){
+        return null;
+    }
+
+    public void createUsernameReenter(ActionEvent actionEvent) {
+    }
+
+    public void createPassword(ActionEvent actionEvent) {
+    }
+
+    public void createPasswordReenter(ActionEvent actionEvent) {
+    }
+
+    public void saveFirstPetQuestion(ActionEvent actionEvent) {
+    }
+
+    public void saveMotherName(ActionEvent actionEvent) {
+    }
 }
--- WritingWizard/src/main/java/com/example/writingwizard/LoginController.java
+++ WritingWizard/src/main/java/com/example/writingwizard/LoginController.java
@@ -1,7 +1,13 @@
 package com.example.writingwizard;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class LoginController {
+    public void password(ActionEvent actionEvent) {
+    }
+
+    public void usernameLogin(ActionEvent actionEvent) {
+    }
 }
