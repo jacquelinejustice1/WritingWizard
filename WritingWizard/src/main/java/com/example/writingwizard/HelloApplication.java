1af333bf2c1ac74d2ec05a6ff7305f4d3c1a992a
Jacqueline Justice <kobrakid3716@gmail.com>
2024-03-17 19:51:07 -0500

    Added some things

--- WritingWizard/src/main/java/com/example/writingwizard/HelloApplication.java
+++ WritingWizard/src/main/java/com/example/writingwizard/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.writingwizard;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
@@ -14,16 +15,17 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        //stage.getIcons().add(new Image("WritingWizard\\Images\\Wizard2.png"));
 
-        //"C:\Users\jjust\Documents\CSCI 4600\WritingWizard\Wizard2.png"
-        Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
-        //scene.getStylesheets().add("C:\\Users\\jjust\\Documents\\CSCI 4600\\WritingWizard\\WritingWizard\\src\\main\\java\\com\\example\\writingwizard\\stylesheet.css");
-        stage.setTitle("Writing Wizard");
-        stage.setScene(scene);
-        stage.show();
 
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+            //stage.getIcons().add(new Image("WritingWizard\\Images\\Wizard2.png"));
+
+            //"C:\Users\jjust\Documents\CSCI 4600\WritingWizard\Wizard2.png"
+            Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
+            //scene.getStylesheets().add("C:\\Users\\jjust\\Documents\\CSCI 4600\\WritingWizard\\WritingWizard\\src\\main\\java\\com\\example\\writingwizard\\stylesheet.css");
+            stage.setTitle("Writing Wizard");
+            stage.setScene(scene);
+            stage.show();
 
         //login in
         FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
--- WritingWizard/src/main/java/com/example/writingwizard/LoginController.java
+++ WritingWizard/src/main/java/com/example/writingwizard/LoginController.java
@@ -2,9 +2,43 @@ package com.example.writingwizard;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+  /*
+    private Stage stage;
+
+  public LoginController(Stage stage){
+      this.stage = stage;
+  }
+    public void switchToMain(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        //stage.getIcons().add(new Image("WritingWizard\\Images\\Wizard2.png"));
+
+        //"C:\Users\jjust\Documents\CSCI 4600\WritingWizard\Wizard2.png"
+        Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
+        //scene.getStylesheets().add("C:\\Users\\jjust\\Documents\\CSCI 4600\\WritingWizard\\WritingWizard\\src\\main\\java\\com\\example\\writingwizard\\stylesheet.css");
+        stage.setTitle("Writing Wizard");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToLogin(ActionEvent actionEvent)  throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),1366, 768);
+        stage.setTitle("Writing Wizard");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+*/
     public void password(ActionEvent actionEvent) {
     }
 
