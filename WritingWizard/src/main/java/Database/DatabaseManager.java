c235233c600e715f88a45187f366b8159712b43a
Jordan Weaver <jweaver20@my.apsu.edu>
2024-04-12 08:37:44 -0500

    Added some function actions in the Manager class and created the DatabaseManager class along with some functions.

---
+++ WritingWizard/src/main/java/Database/DatabaseManager.java
@@ -0,0 +1,14 @@
+package com.example.writingwizard;
+
+
+
+public class DatabaseManager {
+
+    //check if given username exists
+    public boolean userExists(String username) {
+        // if exists, return true
+        // else return false
+    }
+
+    public boolean
+}
--- WritingWizard/src/main/java/com/example/writingwizard/Manager.java
+++ WritingWizard/src/main/java/com/example/writingwizard/Manager.java
@@ -4,74 +4,69 @@ import javafx.scene.control.TextField;
 
 public class Manager {
 
+    User currentuser;
+    String username;
+    String password;
     //Create Account Functions
-    /*
     public String createUser(){
+        // ??
     }
 
-    I need the following functions for alerts on the createAccount page to send to the user
+    //I need the following functions for alerts on the createAccount page to send to the user
 
-    public boolean validateUserName(String username, String usernameReenter){
-     makes sure the username and the username reentered are the same
-     and makes sure that it meets the requirements if we want them
-      (characters long or numbers)
-
-
-     true if the same and meet requirements
-     false if otherwise
-
+    public boolean validateUserNameCreation(String username, String usernameReenter){
+        if (username.equalsIgnoreCase(usernameReenter)) {
+            if (!DatabaseManager.userExists(username)) {
+                this.username = username;
+                return true
+            } else
+                return false
+        } else {
+            return false
+        }
     }
 
-    public boolean validatePassword(String password, String password){
-    password reentered and password are the same
-    true if they are, false if otherwise
+    public boolean validatePasswordCreation(String password, String reEnteredPassword){
+        if (password.equals(reEnteredPassword)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-    public boolean validateSecurityQuestions(String sec1, String sec2, String sec3){
-    returns true if they are all under 20 characters long, contain no numbers maybe
-    otherwise returns false if they are too big and are not real answers
-    }
-
-     */
 
     //Login Functions
-    /*
-    To alert the user if the account is valid or not
-
-
-    public boolean validateLogin(String username, String password){
-    validates user log in
-    returns true if user
-    return false if otherwise
+    //To alert the user if the account is valid or not
+    public boolean validateLogin(String username, String password){ //validates user log in
+        if (DatabaseManager.userExists(username)) {
+            if (DatabaseManager.checkPassword(username, password))
+                return true;
+            else
+                return false;
+        } else
+            return false;
 
     }
 
-    */
-
-    /*
-    View-Only functions
+    //View-Only functions
      public static String openFile(File filePath, TextArea document){
-     opens text file from user that shared the text files to the document
-
+        //opens text file from user that shared the text files to the document
      }
-    Main text editor functions
-    */
 
-    /*
+    //Main text editor functions
     public static String openFile(File filePath, TextArea document){
-    opens a user's already made documents into the textarea to edit
+        //opens a user's already made documents into the textarea to edit
     }
 
     public static void createFile(File filepath, TextArea document){
-    creates a file from the content in the textarea
+        //creates a file from the content in the textarea
     }
 
     public boolean checkIfSaved(???){
-     check to see if the document is saved
-     if saved return true
-     if not saved return false
+        //check to see if the document is saved
+        //if saved return true
+        //if not saved return false
     }
-     */
+
 
     //share screen
 
